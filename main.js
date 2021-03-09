1f7803c6dbaaaecfb6ca5dda82444fd804326edd
Asalef <asalef10@gmail.com>
2021-03-09 21:04:05 +0200

    changeeee

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ let Division = document.getElementById('Division')
 let h1Answer = document.getElementById('h1Answer')
 let numbers = document.getElementById('numbers')
 let reset = document.getElementById('reset')
+let imputTop = document.getElementById('imputTop')
 let i;
 
 
@@ -21,12 +22,19 @@ let arrNumber = []
 
 
 function getInput(num) {
-    
+
     math1.value += num
+    
 }
 
-reset.addEventListener('click',function(){
+function nekuda(tex) {
+    math1.value += tex
+}
+
+reset.addEventListener('click', function () {
     math1.value = ''
+    imputTop.innerHTML = ''
+
 })
 
 EssayButton.addEventListener('click', function () {
@@ -59,68 +67,31 @@ Division.addEventListener('click', function () {
 result.onclick = function () {
 
     if (signe == '+') {
-
+        imputTop.innerHTML += foo12
+        imputTop.innerHTML += signe
+        imputTop.innerHTML += math1.value
         math1.value = Number(math1.value) + Number(foo12)
         // alert('+')
+
+
         console.log(foo12);
     } else if (signe == '*') {
+        imputTop.innerHTML += foo13
+        imputTop.innerHTML += signe
+        imputTop.innerHTML += math1.value
         math1.value = Number(math1.value) * Number(foo13)
 
     } else if (signe == '-') {
+        imputTop.innerHTML += foo14
+        imputTop.innerHTML += signe
+        imputTop.innerHTML += math1.value
         math1.value = Number(foo14) - Number(math1.value)
     } else if (signe == '/') {
+        imputTop.innerHTML += foo15
+        imputTop.innerHTML += signe
+        imputTop.innerHTML += math1.value
         math1.value = Number(foo15) / Number(math1.value)
     }
 }
 
 
-
-
-
-//  let answerEssay = Number(math1.value) + Number(math2.value)
-//  h1Answer.innerHTML = `The Anwer is ${answerEssay}`
-// }
-
-// SubtractionButton.onclick = function EssayFunction(){
-
-//  let answerEssay = Number(math1.value) - Number(math2.value)
-//  h1Answer.innerHTML = `The Anwer is ${answerEssay}`
-// }
-
-// multiplication.onclick = function EssayFunction(){
-
-//  let answerEssay = Number(math1.value) * Number(math2.value)
-//  h1Answer.innerHTML = `The Anwer is ${answerEssay}`
-// }
-
-// Division.onclick = function EssayFunction(){
-
-//  let answerEssay = Number(math1.value) / Number(math2.value)
-//  h1Answer.innerHTML = `The Anwer is ${answerEssay}`
-// }
-
-
-
-// let buttonId1 = document.getElementById('buttonId1')
-
-// buttonId1.onclick = function(){
-//     math1.innerHTML = buttonId1.value
-// }
-
-
-
-
-// for(i =0; i<=10;i++){
-//     foo12 = `<button class="mathNumber" id="numbers${i}">${i}</button>`
-//     foo13 = document.getElementById(`numbers${i}`) 
-// numbers.innerHTML += foo12
-// console.log(i);
-
-// foo = document.getElementById(`numbers${i}`)
-
-
-
-// function color1(){
-// }
-// color1()
-// }
